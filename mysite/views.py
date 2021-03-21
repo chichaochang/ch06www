66a71fff3833dd091c1d9aa479616eef954a39a3
chichaochang <chichao.chang@gmail.com>
2021-03-21 18:08:56 +0800

    Add index.html

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+
+def index(request):
+    now = datetime.now()
+    return render(request, 'index.html', locals())
